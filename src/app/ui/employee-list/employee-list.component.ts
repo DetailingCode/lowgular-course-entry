c2763b05e342b93341d9de4ffda6fee4da2db0c7
DetailingCode <doniek77@o2.pl>
2022-11-19 22:12:03 +0100

    Feat: Lesson 2, add image below name

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -10,6 +10,8 @@ import { EmployeeModel } from '../../model/employee.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeListComponent {
+  title: string = `Sample text`;
+  // data$: Observable<Employee[] | null> = of (null);
   data$: Observable<EmployeeModel[] | null> = this._httpClient.get<EmployeeModel[]>('assets/data/employees.json');
 
   constructor(private _httpClient: HttpClient) {
