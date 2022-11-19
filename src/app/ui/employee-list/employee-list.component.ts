81959bdb4d82ef57970e864ed9c29d467822f439
DetailingCode <doniek77@o2.pl>
2022-11-19 23:11:35 +0100

    Feat: Lesson3, service, httpClient deleted, person model added

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -1,8 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { EmployeeModel } from '../../model/employee.model';
 import { EmployeeService } from '../../services/employee.service';
+import {PersonModel} from "../../model/person.model";
 
 @Component({
   selector: 'employee-list',
@@ -14,7 +13,7 @@ export class EmployeeListComponent {
   title: string = `Sample text`;
   // data$: Observable<Employee[] | null> = of (null);
 
-  data$: Observable<EmployeeModel[] | null> = this._employeeService.getAll();
+  data$: Observable<PersonModel[] | null> = this._employeeService.getAll();
   constructor(private _employeeService: EmployeeService) {
   }
 }
