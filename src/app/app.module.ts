2a7f4b709fb2f5a83c0e0d631d55afcaa3f21b3b
DetailingCode <doniek77@o2.pl>
2022-11-21 19:19:20 +0100

    Feat: Task5 (additional), project service added, app module done,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
 import {FacesComponentModule} from "./ui/faces/faces.component-module";
-import {EmployeeListComponent} from "./ui/employee-list/employee-list.component";
 import {EmployeeServiceModule} from "./services/employee.service-module";
+import {ProjectListComponentModule} from "./ui/project-list/project-list.component-module";
+import {ProjectServiceModule} from "./services/project.service-module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    EmployeeListComponentModule,
-    FacesComponentModule,
-    EmployeeServiceModule,
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        EmployeeListComponentModule,
+        FacesComponentModule,
+        EmployeeServiceModule,
+        ProjectListComponentModule,
+        ProjectServiceModule,
+    ],
   providers: [],
   bootstrap: [AppComponent],
 })
